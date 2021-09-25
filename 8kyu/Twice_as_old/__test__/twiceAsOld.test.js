8b37b0a3ebc148434256d943faa0eb80b5b44daf
Sergey Kudashev <kudashevs@gmail.com>
2021-09-25 11:41:07 +0300

    8kyu: "Twice as old" solved using abs.

---
+++ 8kyu/Twice_as_old/__test__/twiceAsOld.test.js
@@ -0,0 +1,20 @@
+const Test = require('@codewars/test-compat');
+const twiceAsOld = require('./../twiceAsOld');
+
+describe('Sample tests', function () {
+  it("Testing for dad's age: 36 and son's age: 7", function () {
+    Test.assertEquals(twiceAsOld(36, 7), 22);
+  });
+  it("Testing for dad's age: 55 and son's age: 30", function () {
+    Test.assertEquals(twiceAsOld(55, 30), 5);
+  });
+  it("Testing for dad's age: 42 and son's age: 21", function () {
+    Test.assertEquals(twiceAsOld(42, 21), 0);
+  });
+  it("Testing for dad's age: 22 and son's age: 1", function () {
+    Test.assertEquals(twiceAsOld(22, 1), 20);
+  });
+  it("Testing for dad's age: 29 and son's age: 0", function () {
+    Test.assertEquals(twiceAsOld(29, 0), 29);
+  });
+});
