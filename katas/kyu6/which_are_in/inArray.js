27597c9901338c11539be90c8ddfe3235a8c912f
Sergey Kudashev <kudashevs@gmail.com>
2021-10-18 22:15:42 +0300

    6kyu: "Which are in" solved using filter and loop.

---
+++ katas/kyu6/which_are_in/inArray.js
@@ -0,0 +1,14 @@
+function inArray(array1, array2) {
+  return array1
+    .filter((needle) => {
+      for (let hay of array2) {
+        if (hay.includes(needle)) {
+          return true;
+        }
+      }
+      return false;
+    })
+    .sort();
+}
+
+module.exports = inArray;
---
+++ tests/kyu6/which_are_in/inArray.test.js
@@ -0,0 +1,14 @@
+const Test = require('@codewars/test-compat');
+const inArray = require('codewars/katas/kyu6/which_are_in/inArray');
+
+describe('Tests', () => {
+  it('test', () => {
+    let a2 = ['lively', 'alive', 'harp', 'sharp', 'armstrong'];
+    let a1 = ['xyz', 'live', 'strong'];
+    Test.assertSimilar(inArray(a1, a2), ['live', 'strong']);
+    a1 = ['live', 'strong', 'arp'];
+    Test.assertSimilar(inArray(a1, a2), ['arp', 'live', 'strong']);
+    a1 = ['tarp', 'mice', 'bull'];
+    Test.assertSimilar(inArray(a1, a2), []);
+  });
+});
